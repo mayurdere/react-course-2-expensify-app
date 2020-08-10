b0ae86ac9430948bd29526e979e0257d28d4da8c
unknown <mayur.dere7@gmail.com>
2020-08-10 11:09:37 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ const store = configureStore();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters); 
 // console.log(visibleExpenses);
 
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
